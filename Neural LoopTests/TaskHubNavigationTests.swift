c35e915e3de0e47f34057b8b480dc1f1ee989f40
sanjeev halyal <1448731+sanjeevhalyal@users.noreply.github.com>
2026-04-16 11:08:30 +0100

    Add task hub swipe navigation

--- Neural LoopTests/TaskHubNavigationTests.swift
+++ Neural LoopTests/TaskHubNavigationTests.swift
@@ -19,4 +19,65 @@ final class TaskHubNavigationTests: XCTestCase {
         model.select(.todo)
         XCTAssertEqual(model.selectedSection, .todo)
     }
+
+    func testTaskHubLeftSwipeMovesFromTodoToHabits() {
+        let model = TaskHubNavigationModel()
+
+        XCTAssertEqual(model.selectedSection, .todo)
+
+        model.handleSwipe(.left)
+
+        XCTAssertEqual(model.selectedSection, .habits)
+    }
+
+    func testTaskHubRightSwipeMovesFromHabitsToTodo() {
+        let model = TaskHubNavigationModel()
+
+        model.select(.habits)
+        model.handleSwipe(.right)
+
+        XCTAssertEqual(model.selectedSection, .todo)
+    }
+
+    func testTaskHubSwipeAtBoundsKeepsCurrentSection() {
+        let model = TaskHubNavigationModel()
+
+        model.handleSwipe(.right)
+        XCTAssertEqual(model.selectedSection, .todo)
+
+        model.select(.habits)
+        model.handleSwipe(.left)
+        XCTAssertEqual(model.selectedSection, .habits)
+    }
+
+    func testTaskHubSectionAfterSwipeDoesNotMutateSelection() {
+        let model = TaskHubNavigationModel()
+
+        XCTAssertEqual(model.section(afterSwipe: .left), .habits)
+        XCTAssertEqual(model.selectedSection, .todo)
+
+        model.select(.habits)
+        XCTAssertEqual(model.section(afterSwipe: .right), .todo)
+        XCTAssertEqual(model.selectedSection, .habits)
+    }
+
+    func testTodoBackNavigationDoesNothingOnMenu() {
+        let model = TodoViewModel()
+
+        model.viewMode = .menu
+
+        XCTAssertFalse(model.canNavigateBackToMenu())
+        XCTAssertFalse(model.handleBackSwipeIfNeeded())
+        XCTAssertEqual(model.viewMode, .menu)
+    }
+
+    func testTodoBackNavigationReturnsSubViewToMenu() {
+        let model = TodoViewModel()
+
+        model.viewMode = .today
+
+        XCTAssertTrue(model.canNavigateBackToMenu())
+        XCTAssertTrue(model.handleBackSwipeIfNeeded())
+        XCTAssertEqual(model.viewMode, .menu)
+    }
 }
